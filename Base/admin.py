7063fd95dbe065feb037350cc06aad57ead71c82
Kunal0026 <patilkunal699@gmail.com>
2024-08-17 13:41:19 +0530

    initial commit

---
+++ Base/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from Base.models import Contact
+
+# Register your models here.
+admin.site.register(Contact)
+
+
+# Username (leave blank to use 'dell'): Kunal0026
+# Email address: kunalpatil2612001@hmail.com
+# Password:Kunal@2000
